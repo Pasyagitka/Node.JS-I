8d19fd23c6cf5b168bf4c0e26acb6c26deaba4d7
pasyagitka <lizavetazinovich@gmail.com>
2021-09-24 16:04:24 +0300

    04 global module

--- 06/06-04.js
+++ 06/06-04.js
@@ -1,2 +1,3 @@
 const send = require('pasyagitka_6');
+//const send = require('C:/Users/Professional/AppData/Roaming/npm/node_modules/pasyagitka_6');
 send('Сообщение из  скачанного модуля pasyagitka_6');
