b0dddfcbf375beaadd067b7ceaa91bcf5ad85f68
pasyagitka <lizavetazinovich@gmail.com>
2021-12-03 12:17:57 +0300

    15 final

--- 15/15-01.js
+++ 15/15-01.js
@@ -5,7 +5,6 @@ const MongoClient = require('mongodb').MongoClient;
 client = new MongoClient('mongodb+srv://pasyagitka:nodejs@nodejs.prtpp.mongodb.net/BSTU?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 connection = client.connect().then(connection => {console.log('Connection is established'); return connection.db('BSTU')});
 
-//todo ошибки в json формате вернуть
 
 GetCollection = (collection) =>  connection.then(db => {  return db.collection(collection).find({}).toArray()})
 
@@ -33,73 +32,63 @@ DeletePulpit = (pulpit) => connection.then(async db => { return db.collection('p
 
 http.createServer(function(request, response) {
 
-    let requestUrl = url.parse(request.url, true).pathname;
-    response.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    let requestUrl =  decodeURI(url.parse(request.url, true).pathname);
+
+    function successCallback(records) {
+        response.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+        response.end(JSON.stringify(records))
+    }
+    function successUpdateDeleteCallback(records) {
+        response.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+        response.end(records.value != null? JSON.stringify(records.value) : JSON.stringify({message: "no data updated or deleted"}))
+    }
+    function failureCallback(err) {
+        response.writeHead(400, {'Content-Type': 'application/json; charset=utf-8'});
+        response.end(JSON.stringify({error: err.code, message: err.message}))
+    }
 
     if (request.method == 'GET' && requestUrl == '/api/faculties'){
-        GetCollection('faculty')
-        .then(records => response.end(JSON.stringify(records)))
-        .catch(err => response.end(JSON.stringify({error: err.code, message: err.message}))); 
+        GetCollection('faculty').then(successCallback, failureCallback)
     }
     else if (request.method == 'GET' && requestUrl == '/api/pulpits'){
-        GetCollection('pulpit')
-        .then(records => response.end(JSON.stringify(records)))
-        .catch(err => response.end(JSON.stringify({error: err.code, message: err.message}))); 
+        GetCollection('pulpit').then(successCallback, failureCallback)
     }
 
     else if (request.method == 'POST' && requestUrl == '/api/faculties'){
         request.on('data',  data => {
             let newFaculty = JSON.parse(data); 
             InsertFaculty(newFaculty.faculty, newFaculty.faculty_name)
-            .then(result => {
-                GetFacultyById(result.insertedId)
-                .then(result => response.end(JSON.stringify(result)))
-                .catch(err => response.end(JSON.stringify({error: err.code, message: err.message}))); 
-            })
-            .catch(err => response.end(JSON.stringify({error: err.code, message: err.message}))); ;
+            .then(result => {GetFacultyById(result.insertedId).then(successCallback, failureCallback)}, failureCallback)  
         })
     }
     else if (request.method == 'POST' && requestUrl == '/api/pulpits'){
         request.on('data',  data => {
             let newPulpit = JSON.parse(data); 
             InsertPulpit(newPulpit.pulpit, newPulpit.pulpit_name, newPulpit.faculty)
-            .then(result => {
-                GetPulpitById(result.insertedId)
-                .then(result => response.end(JSON.stringify(result)))
-                .catch(err => response.end(JSON.stringify({error: err.code, message: err.message}))); 
-            })
-            .catch(err => response.end(JSON.stringify({error: err.code, message: err.message})));
+            .then(result => {GetPulpitById(result.insertedId).then(successCallback, failureCallback)}, failureCallback)
         });
     }
 
     else if (request.method == 'PUT' && requestUrl == '/api/faculties'){
         request.on('data',  data => {
             let newFaculty = JSON.parse(data); 
-            UpdateFaculty(newFaculty.faculty, newFaculty.faculty_name)
-            .then(records =>  response.end(records.value != null? JSON.stringify(records.value) : JSON.stringify({message: "no faculty updated"})))
-            .catch(err => response.end(JSON.stringify({error: err.code, message: err.message}))); 
+            UpdateFaculty(newFaculty.faculty, newFaculty.faculty_name).then(successUpdateDeleteCallback, failureCallback)
         })
     }
     else if (request.method == 'PUT' && requestUrl == '/api/pulpits'){
         request.on('data',  data => {
             let newPulpit = JSON.parse(data); 
-            UpdatePulpit(newPulpit.pulpit, newPulpit.pulpit_name, newPulpit.faculty)
-            .then(records => response.end(records.value != null? JSON.stringify(records.value) : JSON.stringify({message: "no pulpit updated"})))
-            .catch(err => response.end(JSON.stringify({error: err.code, message: err.message})));   
+            UpdatePulpit(newPulpit.pulpit, newPulpit.pulpit_name, newPulpit.faculty).then(successUpdateDeleteCallback, failureCallback)
         });
     }
 
-    else if (request.method == 'DELETE' && (/\/api\/faculties\/\w+/).test(requestUrl)){
+    else if (request.method == 'DELETE' && (/\/api\/faculties\/[А-Яа-яA-Za-z]/).test(requestUrl)){
         let id = url.parse(requestUrl, true).pathname.split('/')[3];
-        DeleteFaculty(id)
-        .then(records => response.end(records.value != null? JSON.stringify(records.value) : JSON.stringify({message: "no faculty deleted"})))   
-        .catch(err => response.end(JSON.stringify({error: err.code, message: err.message}))); 
+        DeleteFaculty(id).then(successUpdateDeleteCallback, failureCallback)   
     }
-    else if (request.method == 'DELETE' && (/\/api\/pulpits\/\w+/).test(requestUrl)){
+    else if (request.method == 'DELETE' && (/\/api\/pulpits\/[А-Яа-яA-Za-z]/).test(requestUrl)){
         let id = url.parse(requestUrl, true).pathname.split('/')[3];
-        DeletePulpit(id)
-        .then(records =>  response.end(records.value != null? JSON.stringify(records.value) : JSON.stringify({message: "no pulpit updated"}))) 
-        .catch(err => response.end(JSON.stringify({error: err.code, message: err.message}))); 
+        DeletePulpit(id).then(successUpdateDeleteCallback, failureCallback) 
     }
 
     else {
